5a86effbe711b94a3929a046e73613af58f9ce06
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-18 15:54:47 -0700

    Add files via upload
    update again

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -51,26 +51,26 @@ for (property in newAnimal) {
 }
 */
 
-// Step 7
-
+// Step 7, 8
+/*
 function Animal(color) {
     "use strict";
+   // var color = color;
     this.color = color;
-    Animal.prototype.speak = function () {
-        return this.color;
-    };
+    //Animal.prototype.speak = function () {
+      //  return this.color;
+    //};
     var speak = function () {
         if (this.color === "red") {
-            return "The red cat is meowing!";
-        } else {
-            return "The blue dog is barking!";
+            window.console.log("The red cat is meowing!");
+        } else if (this.color === "blue") {
+            window.console.log("The blue dog is barking!");
         }
     };
 }
-window.console.log(this.speak);
+window.console.log(this.color("red"));
 
-/*
-Animal.prototype.speakAnimal = function () {
+/* Animal.prototype.speakAnimal = function () {
     "use strict";
     if (this.speak === "dog") {
         return "The <color> dog is barking!";
@@ -82,5 +82,17 @@ Animal.prototype.speakAnimal = function () {
 };
 window.console.log(this.speak); */
 
-//  var cat = new Animal("red");
-//  var dog = new Animal("blue");
+// Step 9 
+
+var findWords = "animal";
+var close = "I don't wanna be somebody without your body close to me. And if it wasn't you, I wouldn't want anybody close to me, Cause I'm an animal, animal, like animal, like you."
+var i = 0;
+var occur = -1;
+var j = -1;
+
+while (i != -1) {
+    i = close.indexOf(findWords, j + 1);
+    occur =+ 1;
+    j = i;
+}
+window.alert("There were " + occur + " occurrences of " + findWords + " in the paragraph.");
